f1dd2f7556a70a3f37443168dcb1db0032d2cf2b
sreejathoom <sreejathoom@gmail.com>
2019-03-21 16:20:57 -0700

    Merge Initial Import

--- IndoorLocator/app/src/main/java/com/locateme/indoor_locator/ApDataScan.java
+++ IndoorLocator/app/src/main/java/com/locateme/indoor_locator/ApDataScan.java
@@ -10,33 +10,6 @@ import android.widget.TextView;
 
 import java.util.List;
 
-public class ApDataScan extends AppCompatActivity {
-    private EditText tvWifiSignalLevel;
-    private WifiManager myWifiManager;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ap_data_scan);
-
-        tvWifiSignalLevel = (EditText) findViewById(R.id.wifi_signal_level_label);
-
-        myWifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-        myWifiManager.setWifiEnabled(true);
-        System.out.print("Hello");
-        if (myWifiManager.startScan()) {
-            List<ScanResult> scans = myWifiManager.getScanResults();
-            if (scans != null && !scans.isEmpty()) {
-                for (ScanResult scan : scans) {
-                    int level = WifiManager.calculateSignalLevel(scan.level, 20);
-                    int lev = scan.level;
-                    String SSID = scan.SSID;
-                    String BSSID = scan.BSSID;
-                    System.out.print(lev);
-                    tvWifiSignalLevel.setText(lev);
-                }
-            }
-        }
-    }
+public class ApDataScan  {
 
 }
